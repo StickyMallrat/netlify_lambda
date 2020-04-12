f411af2246c6ca61c55fe72eee584133b68169b7
Fabian Mallmann <fabian.mallmann@googlemail.com>
2020-04-13 01:10:47 +0200

    Implemented button functionality

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 const fetchUsers = async () => 
     await (await fetch('/.netlify/functions/getusers')).json();
 
-fetchUsers().then(data => {
-    userList = document.querySelector('#users');
+const button = document.getElementById('button');
 
-    data.forEach(user => {
-        const li = document.createElement('li');
-        li.className = 'list-group-item';
-        const link = document.createElement('a');
-        link.appendChild(document.createTextNode(user.login));
-        link.href = user.html_url;
-        link.target = 'blank';
-        li.appendChild(link);
-        userList.appendChild(li);
+button.onclick = () => {
+    fetchUsers().then(data => {
+        userList = document.querySelector('#users');
+    
+        data.forEach(user => {
+            const li = document.createElement('li');
+            li.className = 'list-group-item';
+            const link = document.createElement('a');
+            link.appendChild(document.createTextNode(user.login));
+            link.href = user.html_url;
+            link.target = 'blank';
+            li.appendChild(link);
+            userList.appendChild(li);
+        });
     });
-});
+}
